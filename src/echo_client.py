8a3b4ae4a2e297def3b7d6099254f02da810cdc2
Haram <haram.dev.09@gmail.com>
2020-06-15 15:52:35 +0900

    added echo server and client

--- src/echo_client.py
+++ src/echo_client.py
@@ -9,17 +9,34 @@
 - 기본 Factory 클래스는 Protocol 클래스 객체를 생성하여, 해당 객체의 factory 변수가 그 Factory 클래스를 가리키도록 한다.
 - Protocol 클래스에 접근하고, 환경 설정을 할 수 있게 된다.
 """
+from twisted.internet import reactor, protocol
 
-import socket
+
+class EchoClient(protocol.Protocol):
+    def connectionMade(self):
+        self.transport.write("Hello, world!")
+
+    def dataReceived(self, data):
+        print "Server said:", data
+        self.transport.loseConnection()
+
+
+class EchoFactory(protocol.ClientFactory):
+    def buildProtocol(self, addr):
+        return EchoClient()
+
+    def clientConnectionFailed(self, connector, reason):
+        print "Connection failed."
+        reactor.stop()
+
+    def clientConnectionLost(self, connector, reason):
+        print "Connection lost."
+        reactor.stop()
 
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(('127.0.0.1', 1234))
-        s.sendall(b'Hello, world')
-        data = s.recv(1024)
-
-        print('Received', data.decode())
+    reactor.connectTCP("localhost", 8000, EchoFactory())
+    reactor.run()
 
 
 if __name__ == "__main__":
--- src/echo_server.py
+++ src/echo_server.py
@@ -1,23 +1,21 @@
 # -*- coding: utf-8 -*-
 
 
-from twisted.internet import protocol, reactor, endpoints
+from twisted.internet import protocol, reactor
 
 
 class Echo(protocol.Protocol):
     def dataReceived(self, data):
-        # echo 클라이언트로부터 바이트 형태의 데이터를 받는 함수
-        self.transport.write(data + "!!!".encode())
+        self.transport.write(data)
 
 
 class EchoFactory(protocol.Factory):
     def buildProtocol(self, addr):
-        # Echo 함수를 반환하는(객체 아닌가?).. 프로토콜 팩토리 함수
         return Echo()
 
 
 def main():
-    endpoints.serverFromString(reactor, "tcp:1234").listen(EchoFactory())
+    reactor.listenTCP(8000, EchoFactory())
     reactor.run()
 
 
